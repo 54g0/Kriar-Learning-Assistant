f348ff90886c820416085e2b9438399bdf61ab62
SAGAR <sagar8296d@gmail.com>
2025-09-07 23:30:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,1 @@
-# pip install yt-dlp
-from yt_dlp import YoutubeDL
 
-def ytdlp_metadata(url: str) -> dict:
-    opts = {}
-    with YoutubeDL(opts) as ydl:
-        info = ydl.extract_info(url, download=False)
-    # Select common fields; info contains many more keys
-    return {
-        "id": info.get("id"),
-        "title": info.get("title"),
-        "uploader": info.get("uploader"),
-        "channel": info.get("channel"),
-        "channel_id": info.get("channel_id"),
-        "duration": info.get("duration"),
-        "description": info.get("description"),
-    }
-
-print(ytdlp_metadata("https://www.youtube.com/watch?v=Gu1LpctEYZA&t=2477s"))
